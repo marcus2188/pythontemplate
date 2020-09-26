2ff2cdd891821969d9cc17a280460de609675a7d
marcus2188 <marcus.2188@gmail.com>
2020-09-26 15:37:01 +0800

    g

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask;
 app = Flask(__name__)
 import codeitsuisse.routes.square
-import codeitsuisse.routes.salad-spree
+import codeitsuisse.routes.revisitgeometry
 
 
 
--- codeitsuisse/routes/revisitgeometry.py
+++ codeitsuisse/routes/revisitgeometry.py
@@ -7,28 +7,53 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-@app.route('/salas-spree', methods=['POST'])
-def evaluate_salad():
+@app.route('/revisitgeometry', methods=['POST'])
+def evaluate_geo():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    n = data.get("number_of_salads");
-    fff = data.get("salad_prices_street_map");
-    min = 999
-    result = 0
-    for x in fff:
-        sumw = 0
-        if len(x) >= n:
-            for index in range(0, len(x)-(n-1)):
-                boollist = [x[lol].isdigit() for lol in range(index,index+n)]
-                if False not in boollist:
-                    sumw = sum([int(x[lol]) for lol in range(index,index+n)])
-                    if sumw < min:
-                        min = sumw
-                sumw = 0
-    if min == 999:
-        result = 0
-    else:
-        result = min
+    shapecoords = data.get("shapeCoordinates");
+    linecoords = data.get("lineCoordinates");
+    line_grad = float(linecoords[1]["y"] - linecoords[0]["y"]) / float(linecoords[1]["x"] - linecoords[0]["x"])
+    line_c = linecoords[1]["y"] - line_grad * linecoords[1]["x"]
+    result = []
+    for i in range(0, len(shapecoords)-1):
+        for j in range(i+1, len(shapecoords)):
+            minx = min([shapecoords[j]["x"], shapecoords[i]["x"]])
+            maxx = max([shapecoords[j]["x"], shapecoords[i]["x"]])
+            miny = min([shapecoords[j]["y"], shapecoords[i]["y"]])
+            maxy = max([shapecoords[j]["y"], shapecoords[i]["y"]])
+            if shapecoords[j]["y"] - shapecoords[i]["y"] == 0:
+                y_val = shapecoords[j]["y"]
+                x_val = (y_val - line_c)/ (line_grad)
+                if isinstance(x_val, float) and x_val.is_integer():
+                    x_val = int(x_val)
+                if isinstance(y_val, float) and y_val.is_integer():
+                    y_val = int(y_val)
+                if x_val >= minx and x_val <= maxx and y_val >= miny and y_val <= maxy:
+                    result.append({"x":round(x_val,2), "y":round(y_val,2)})
+                
+            elif shapecoords[j]["x"] - shapecoords[i]["x"] == 0:
+                x_val = shapecoords[j]["x"]
+                y_val = line_grad * x_val + line_c
+                if isinstance(x_val, float) and x_val.is_integer():
+                    x_val = int(x_val)
+                if isinstance(y_val, float) and y_val.is_integer():
+                    y_val = int(y_val)
+                if x_val >= minx and x_val <= maxx and y_val >= miny and y_val <= maxy:
+                    result.append({"x":round(x_val,2), "y":round(y_val,2)})
+                
+            else:
+                shapeline_grad = (shapecoords[j]["y"] - shapecoords[i]["y"]) / (shapecoords[j]["x"] - shapecoords[i]["x"])
+                shapeline_c = shapecoords[j]["y"] - shapeline_grad * shapecoords[j]["x"]
+                x_val = (shapeline_c - line_c) / (line_grad - shapeline_grad)
+                y_val = x_val * shapeline_grad + shapeline_c
+                if isinstance(x_val, float) and x_val.is_integer():
+                    x_val = int(x_val)
+                if isinstance(y_val, float) and y_val.is_integer():
+                    y_val = int(y_val)
+                if x_val >= minx and x_val <= maxx and y_val >= miny and y_val <= maxy:
+                    result.append({"x":round(x_val,2), "y":round(y_val,2)})
+                
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
